a1e3ac4e1ae0a84851b9eb0325a374b2dfc9b1bc
Husam0865 <h2705518@gmail.com>
2026-04-29 05:11:26 +0300

    update

--- Product.h
+++ Product.h
@@ -5,11 +5,26 @@
 #include <string>
 using namespace std;
 
-class Product {
+class Product
+{
 protected:
     int id;
     string name;
     double price;
     int quantity;
+
+public:
+    Product(int id, string name, double price, int quantity);
+    virtual ~Product();
+
+    // Getters
+    int getId() const;
+    string getName() const;
+    double getPrice() const;
+    int getQuantity() const;
+
+    // Setters
+    void setPrice(double p);
+    void setQuantity(int q);
 };
-#endif 
+#endif
