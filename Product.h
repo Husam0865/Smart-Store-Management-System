8a507e99b1bc2043afeff44d7c7f624065109218
Husam0865 <h2705518@gmail.com>
2026-04-29 04:37:55 +0300

    update Product.h

---
+++ Product.h
@@ -0,0 +1,15 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Product {
+protected:
+    int id;
+    string name;
+    double price;
+    int quantity;
+};
+#endif 
