297d1a81ccbcf790b9d053642e9d9d2e1ffe5bb9
Husam0865 <h2705518@gmail.com>
2026-04-29 06:20:01 +0300

    add office methods

--- OfficeSupply.h
+++ OfficeSupply.h
@@ -3,9 +3,23 @@
 
 #include "Product.h"
 
-class OfficeSupply : public Product {
+class OfficeSupply : public Product
+{
 private:
     string category;
     string material;
+
+public:
+    OfficeSupply(int id, string name, double price, int quantity,
+                 string category, string material);
+    // Setters
+    void setCategory(const string &c);
+    void setMaterial(const string &m);
+    // Getters
+    string getCategory() const;
+    string getMaterial() const;
+    // Override virtual functions
+    string getType() const override;
+    void display() const override;
 };
 #endif
