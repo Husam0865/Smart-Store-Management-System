a0969d19c49ef026d803268478fc525bc4d9dc36
Husam0865 <h2705518@gmail.com>
2026-04-29 06:12:58 +0300

    add book methods

--- Book.h
+++ Book.h
@@ -3,9 +3,23 @@
 
 #include "Product.h"
 
-class Book : public Product {
+class Book : public Product
+{
 private:
     string author;
     int pages;
+
+public:
+    Book(int id, string name, double price, int quantity,
+         string author, int pages);
+    // Setters
+    void setAuthor(const string &a);
+    void setPages(int p);
+    // Getters
+    string getAuthor() const;
+    int getPages() const;
+    // Override virtual functions
+    string getType() const override;
+    void display() const override;
 };
 #endif
